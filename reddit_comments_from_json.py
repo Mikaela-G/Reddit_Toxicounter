15bfd6885571216dfe6db48295736b023985195d
Minh Hua <42155133+duyminh1998@users.noreply.github.com>
2018-11-27 20:35:02 -0800

    script to transfer data from json

---
+++ reddit_comments_from_json.py
@@ -0,0 +1,25 @@
+import json
+import pandas as pd
+
+filename = 'RC_2005-12' # input your file name here
+subreddit_list = ['AskReddit', 'politics', 'The_Donald', 'worldnews', 'nba', 'videos', 'funny', 'todayilearned', 'soccer', 'CFB'] 
+
+comments = []
+
+for line in open(filename, 'r'):
+    comments.append(json.loads(line))
+
+dictkeys = list(comments[0].keys())
+
+df = pd.DataFrame(columns = dictkeys)
+j = 0
+for subreddit_name in subreddit_list:
+    for i in range(len(comments)):
+        if comments[i]['subreddit'] == subreddit_name:
+            for key in dictkeys:
+                df.at[j, key] = comments[i][key]
+            j += 1
+        else:
+            pass
+
+df.to_csv(filename + '.csv') # the output is your subreddit's name + csv
