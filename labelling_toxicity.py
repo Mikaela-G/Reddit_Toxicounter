14240c3b1a8c98330d92f5e18526932e25835167
Mikaela-G <32914691+Mikaela-G@users.noreply.github.com>
2019-01-27 19:48:19 -0800

    Update labelling_toxicity.py

--- labelling_toxicity.py
+++ labelling_toxicity.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jan  5 15:24:40 2019
@@ -11,6 +11,8 @@ Created on Sat Jan  5 15:24:40 2019
 # ADD toxic_score REAL;
 # ALTER TABLE AskReddit
 # ADD severe_toxicity REAL;
+# ALTER TABLE AskReddit
+# ADD toxic_label VARCHAR;
 
 import json
 import requests
@@ -40,11 +42,20 @@ def sql_update_2(severe_toxicity, comment_id):
         transaction_bldr(sql)
     except Exception as e:
         print('SQL insertion',str(e))
+
+def sql_update_3(real_toxic_label, comment_id):
+    try:
+        sql = "UPDATE '{}' SET real_toxic_label = '{}' WHERE comment_id = '{}';".format(subreddit_name, real_toxic_label, comment_id)
+        # transaction_bldr(sql)
+        c.execute(sql)
+        connection.commit()
+    except Exception as e:
+        print('SQL insertion',str(e))
         
 def transaction_bldr(sql):
     global sql_transaction
     sql_transaction.append(sql)
-    if len(sql_transaction) >= 10:
+    if len(sql_transaction) >= 200:
         c.execute('BEGIN TRANSACTION')
         for s in sql_transaction:
             try:
@@ -76,3 +87,19 @@ def main():
         
 if __name__ == '__main__':
 	main()
+
+# redefine threshold to account for severe_toxicity more?; create histograms to show distribution?
+c.execute('SELECT comment_id, comment, toxic_score, severe_toxicity FROM {}'.format(subreddit_name)) # WHERE unix > ___
+results = c.fetchall()
+for result in (comment for comment in results):
+    if result[2]>=.7:
+        if result[3]>=.7:
+            real_toxic_label = "very toxic"
+        elif result[3]<=.7:
+            real_toxic_label = "moderately toxic"
+    elif result[2]>=.4 and result[2]<.7:
+        real_toxic_label = "moderately toxic"
+    elif result[2]<.4:
+        real_toxic_label = "not toxic"
+    # print(toxic_label)
+    sql_update_3(real_toxic_label, result[0])
