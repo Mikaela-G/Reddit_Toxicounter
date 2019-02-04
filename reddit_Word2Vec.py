eb47b47564bf89f5e4b71e8896a48b866f499daa
Mikaela-G <32914691+Mikaela-G@users.noreply.github.com>
2019-02-03 21:31:02 -0800

    Update reddit_Word2Vec.py

--- reddit_Word2Vec.py
+++ reddit_Word2Vec.py
@@ -24,14 +24,53 @@ df['comment_tok'] = df['comment'].apply(lambda x: nltk.word_tokenize(x))
 model = Word2Vec(df.comment_tok, sg = 1) # skip gram
 print(model)
 
+# gets vocabulary
 words = list(model.wv.vocab)
 print(words)
 
-# saves model so it can be re-used
-model.save('model.bin')
-# new_model = Word2Vec.load('--insert model filepath here--')
-# print(new_model)
+# saves model's word vectors so they can be re-used
+model.wv.save_word2vec_format('model_wv.txt', binary=False)
+# model.save('model.bin')
+# model = Word2Vec.load('--insert model filepath here--')
+# print(model)
 
+
+
+
+
+
+
+# we're making two types of models:
+#   3 part classification models that predict toxicity label from word embeddings (and nothing else)
+#       3 potential labels:
+#           very toxic -> >90% toxic_score and >70% severe_toxicity
+#           moderately toxic -> <90% & >50% toxic_score and <70% & >50% severe_toxicity
+#           not toxic -> <50% toxic_score
+#           (see if there's any cases where there's <50% toxic_score but it's also severely toxic)
+#       algorithms we can use:
+#           Naive Bayes Classifier 
+#           Logistic Regression
+#           Random Forest
+#           SVM
+#           Recurrent Neural Network
+#           Nearest Neighbor
+#   regression models that analyze the interaction between toxic_label, time, and score
+
+# things we need to do before modeling:
+#   create 2 new columns: severe_toxicity, toxic_label
+#       (toxicity_label will be our new ground truth)
+#   create word embeddings using Word2Vec
+
+# for the first type of model:
+#   input for the main model = word embeddings; other linguistic information (like number of uppercase or punctuation or etc.)
+#   output for the main model = toxicity label
+# for the second type of model:
+#   exploring the relationships between the predictors and dependent variable
+#   dependent variable can be toxicity_label, if we want to see how time and score impact toxicity
+
+# stuff for visualization rather than modeling:
+#   relationship between time and toxicity_label
+#   relationship between time and score
 
 
 
